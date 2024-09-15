ddb2bca853bdf418e24c426c0dc0ef6ce4f95e1a
fifibox <167882976+fifibox@users.noreply.github.com>
2024-09-15 19:46:18 +0800

    Update day2.py

--- day2.py
+++ day2.py
@@ -34,8 +34,38 @@ with open("input.txt","r") as file:
                 
 print(total)
 
-#---part 2 placeholder
+
+#---part 2 -------------------------------
+
+total = 0
+game_id = 0
+
+with open("input.txt","r") as file:
+    for game in file:
         
+        # Reset bag for each game 
+        bag = {'red':1,'green':1,'blue':1}
+        
+        # convert each game to a list of 3 sets
+        game = game.split(':')[1].strip().split(';')
+
+        for set in game:
+            
+            # convert each set to a list of cube number and color  
+            set = set.split(',')
+            set = [i.lstrip().split() for i in set]
+            
+            # check the number of each colour against bag dictionary 
+            for cube in set:
+                
+                # update number in bag if cube number is higher than number in bag for that color.
+                if int(cube[0]) > bag.get(cube[1]):
+                    bag[cube[1]] = int(cube[0])
+        
+        power = bag['red'] * bag['green'] * bag['blue']
+        total += power
+            
+print(total)
                     
 
 
